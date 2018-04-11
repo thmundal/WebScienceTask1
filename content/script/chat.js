9e9d8235ee8fe985deef9ae1de7b65f67e0f91f5
Thomas Mundal <thmundal@gmail.com>
2018-04-11 19:48:50 +0200

    Include partner as a data value for chat view

--- content/script/chat.js
+++ content/script/chat.js
@@ -27,13 +27,31 @@ $(function() {
                     if(handle.view === null) {
                         $(this).append(handle.createView());
                     }
-                })
+
+                    if(h == "") {
+                        console.log("* View has no handle defined, finding based on partner");
+                        var p = $(this).data("partner");
+                        for(var i in chat.handles) {
+                            if(chat.handles[i].attributes.a == p || chat.handles[i].attributes.b == p) {
+                                $(this).append(chat.handles[i].createView());
+                                $(this).data("handle", chat.handles[i].attributes.id);
+                            }
+                        }
+                    }
+                });
             });
 
             this.getChatHandles();
         });
 
         chat.connect("munso.no:3000");
+
+
+        $(".js-chat-view-container").each(function() {
+            if($(this).data("handle") == "") {
+                chat.createHandle(user_id, $(this).data("partner"));
+            }
+        });
 
     }
 });
